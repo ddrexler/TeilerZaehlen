fa24896631387066cb74ee51b918e7e3ff9d63bb
David Drexler <david.drexler.dd@gmail.com>
2018-05-11 16:17:37 +0200

    first commit

---
+++ IntOverflow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = Int32.MinValue;
+            int max = Int32.MaxValue;
+            int errorInt = Convert.ToInt32("2147483648");
+
+            Console.WriteLine("{0}, {1}", min, max);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TeilerFinden/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TeilerFinden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groessteZahl = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Bitte geben Sie eine positive Ganzzahl ein! (Leere Eingabe beendet das Programm)");
+
+                try
+                {
+                    string input = Console.ReadLine();
+                    if (input.Equals("")) break;
+                    int inputNumber = Convert.ToInt32(input);
+
+                    if (inputNumber < 1)
+                    {
+                        Console.WriteLine("Die Zahl muss positiv sein!");
+                        continue;
+                    }
+
+                    if (inputNumber > groessteZahl) groessteZahl = inputNumber;
+
+                    int summe = 0;
+                    int anzahlTeiler = 0;
+                    //Ausgabe der Teiler
+                    Console.Write("Teiler: ");
+                    for (int i = inputNumber; i > 0; i--)
+                    {
+                        if (inputNumber % i == 0)
+                        {
+                            if(i == 1)
+                            {
+                                Console.WriteLine(i);
+                            }
+                            else
+                            {
+                                Console.Write("{0}, ", i);
+                            }
+                            summe += i;
+                            anzahlTeiler += 1;
+                        }
+                    }
+                    Console.WriteLine("Summe der Teiler: {0}", summe);
+                    Console.WriteLine("Anzahl Teiler: {0}", anzahlTeiler);
+                    Console.WriteLine("Die groesste Zahl war: {0}", groessteZahl);
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Diese Eingabe ist keine Zahl!");
+                }
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("Die Zahl ist zu gross!");
+                }
+
+
+            }
+        }
+    }
+}
